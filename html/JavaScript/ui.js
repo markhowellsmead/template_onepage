bfc01974b07f0e57064d3a874aa9b41fea1ad545
Mark Howells-Mead <permanent.tourist@gmail.com>
2015-10-14 15:34:30 +0200

    FIner details

---
+++ html/JavaScript/ui.js
@@ -0,0 +1,43 @@
+(function($, undefined){
+
+	var anchorAnimateOffset;
+
+	$.extend($.fn,{
+
+		anchorAnimate: function() {
+			return this.each(function(){
+				$(this).bind('click', function(e){
+					var targetAnchor = $(this).attr('href').split('#');
+					var destination = $('#'+targetAnchor[1]);
+					if(destination && destination.length){
+						e.preventDefault();
+						if( $(destination).hasClass('is-folded') ){
+							$(destination).toggleClass('unfolded');
+						}
+						$(destination).scrollToMe();
+					}
+				});
+			});
+		},
+
+		scrollToMe : function(){
+			var destination = $(this);
+			if(destination && destination.length){
+				var destinationTop = destination.offset().top;
+				if(anchorAnimateOffset){
+					destinationTop = destinationTop - anchorAnimateOffset;
+				}
+				$("html:not(:animated),body:not(:animated)").animate({ scrollTop: Math.floor(destinationTop)}, 600);
+			}
+			return this;
+		}
+
+	});
+
+	//////////////////////////////////////////////////
+
+	$(document).on('ready', function(){
+		$('a.anchor').anchorAnimate();
+	});
+
+})(jQuery);
